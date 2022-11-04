21fe6a191dfd861ef8af4cc31643653a519ab3f3
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2022-11-04 07:06:15 +0100

    google analytics

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -1,8 +1,13 @@
 const User= require('../Models/adminModel')
 const AppError= require('../utils/appError')
+const google= require('googleapis')
+const scopes= 'https://www.googleapis.com/auth/analytics.readonly'
+const jwt= new google.auth.JWT(process.env.CLIENT_EMAIL, null, process.env.PRIVATE_KEY,scopes)
+const view_id= 340951165
 
 
-exports.createVisitor= async(req, res, next)=>{
+
+exports.createAdmin= async(req, res, next)=>{
         const admin= await User.create(req.body)
 
         if(!admin) return next(new(AppError('something went wrong', 400)))
@@ -13,4 +18,31 @@ exports.createVisitor= async(req, res, next)=>{
         })
 
     
+};
+
+
+exports.googleAnalytics= async(req, res, next)=>{
+  try{
+    const result= await google.analytics('v3').data.ga.get({
+      'auth': jwt,
+      'ids': 'ga:'+ view_id,
+      'start-date': '30daysAgo',
+      'end-date':'today',
+      'metrics': 'ga:pageviews'
+    })
+    console.log(result)
+    res.status(200).json({
+      status: 'success',
+      data: result
+
+    })
+  }catch(err){
+    res.status(500).json({
+      status: "fail",
+      data: err
+    })
+
+  }
 }
+
+  
--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -5,8 +5,9 @@ const authController= require('../Controllers/authController')
 
 const router= express.Router();
 
-router.route('/sign-up').post(adminController.createVisitor)
+router.route('/sign-up').post(adminController.createAdmin)
 router.route('/login').post(authController.login)
+router.route('/analytics').get(adminController.googleAnalytics)
 
 
 module.exports= router;
