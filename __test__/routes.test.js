0638aa3184cab1883bc97d4e32547bf7aaace0c8
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2022-12-15 18:52:16 +0100

    all tests passes

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -2,10 +2,10 @@ const request = require('supertest')
 const mongoose= require('mongoose')
 const app= require('../app')
 const dotenv= require('dotenv');
-const login= require('./login');
-const {mockAxios}= require('axios');
+const authController= require('../Controllers/authController');
+const express= require('express');
 
-
+const router= express.Router();
 
 dotenv.config({path: './config.env'});
 
@@ -23,30 +23,32 @@ const signupDetails= {
     "confirmPassword": "kunle1374"
 };
 
-function forEach(items, callback) {
-    for (let index = 0; index < items.length; index++) {
-      callback(items[index]);
-    }
-  };
-
-
-beforeAll(() => {
-    mongoose.connect(DB, {
-        useNewUrlParser:true,
-        useUnifiedTopology: true
-    }).then(()=> console.log('DB connection successful'))     
-  });
-
-afterAll(async ()=>{
-    await mongoose.disconnect();
-    await mongoose.connection.close()
-})
 
 describe('admin route testing', ()=>{
-     
+    let protectMocked
+
+    beforeAll(() => {
+        mongoose.connect(DB, {
+            useNewUrlParser:true,
+            useUnifiedTopology: true
+        }).then(()=> console.log('DB connection successful'));
+
+        // .mockImplementation(()=>{
+        //     return 'fake logged in'
+        // });   
+        // protectMocked= jest.spyOn(authController, 'protect')
+        // .mockReturnValue('next()');    
+    });
+
+    afterAll(async ()=>{
+        await mongoose.disconnect();
+        await mongoose.connection.close();
+
+        jest.clearAllMocks();
+    });
+
     describe('loging in admin', ()=>{
         describe('testing log in', ()=>{
-
             //Login the admin if the correct details was given       
             it('should send 200 statusCode and login the user', async()=>{  
                 const {body, statusCode} = await request(app).post('/api/v1/admin/login')
@@ -57,6 +59,7 @@ describe('admin route testing', ()=>{
 
             //Testing if wrong email or password was provided. 
             it('send 400 statusCode for wrong user name or password', async()=>{
+                
                 const {statusCode} = await request(app).post('/api/v1/admin/login')
                 .send({...loginDetails, password: "kunle112"});
 
@@ -70,11 +73,19 @@ describe('admin route testing', ()=>{
     describe('signing up admin', ()=>{
         describe('sucessful sign-up', ()=>{
             it('signing-up admin and deleting admin', async()=>{
+
+                //login so the delete route can be accesed
+                const {body:loginBody} = await request(app).post('/api/v1/admin/login')
+                .send({...loginDetails});
+    
+                //signing up data
                 const {body, statusCode}= await request(app)
                 .post('/api/v1/admin/sign-up')
                 .send(signupDetails)
-                console.log(body)
+
+                //deleting the same document from the database.
                 const {statusCode:deleteStatus}= await request(app).delete(`/api/v1/admin/delete/${body.data._id}`)
+                .set('Cookie', [`secretoken=${loginBody.token}`]);
 
                 expect(statusCode).toBe(201)
                 expect(deleteStatus).toBe(200)
@@ -84,40 +95,37 @@ describe('admin route testing', ()=>{
     });
 
     
-    //Testing the users valid Id
+    //Testing Protected Routes
 
     describe('get user route', ()=>{
+
         describe("give a user doesn't exist",()=>{
-        it('should return a 404 statusCode if there is no user with this id', async()=>{
+            
+        it('should return a 404 statusCode if there is no user with this id', async()=>{ 
             const fakeUser= '636f2ecd4e497fc31864b6f3'
 
-            mockAxios.get.mockImplementationOnce(()=> Promise.resolve({
-                data: {results: ["cat.jpg"]}
-                
-            })) 
+            const {body} = await request(app).post('/api/v1/admin/login')
+            .send({...loginDetails});
 
-            // const acceptes
+            const{statusCode}= await request(app).get(`/api/v1/user/${fakeUser}`)
+            .set('Cookie', [`secretoken=${body.token}`]);
 
-            const{body, statusCode}= await request(app).get(`/api/v1/user/${fakeUser}`);
-
-            expect(statusCode).toBe(404)
-            expect(mockAxios.get).toHaveBeenCalled()
+            expect(statusCode).toBe(404);
         })
 
         it('should return a 200 statusCode and a data', async()=>{
-            const user= '636f2ecd4e497fb31864b6f1'
-            const {body, statusCode}= await request(app).get(`/api/v1/user/${user}`);
+            const user= '636f2ecd4e497fb31864b6f1';
+
+            const {body} = await request(app).post('/api/v1/admin/login')
+            .send({...loginDetails});
+
+            const {body:Body,statusCode}= await request(app).get(`/api/v1/user/${user}`)
+            .set('Cookie', [`secretoken=${body.token}`]);
 
             expect(statusCode).toBe(200)
-            expect(body.data._id).toBe(user)
+            expect(Body.data._id).toBe(user)
         });
 
-        // it('testing a call back function', ()=>{
-        //     const mockCallback= jest.fn(authController.protect)
-        //     forEach([0, 1], mockCallback);
-
-        //     expect(mockCallback.mock.calls.length).toBe(2);
-        // })
 
     });
 
