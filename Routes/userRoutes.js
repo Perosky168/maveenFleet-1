be74000b9fefe2c1a7cb44b2ec555b3466150097
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2022-12-18 11:50:00 +0100

    user routes

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -5,5 +5,7 @@ const userController= require('../Controllers/userController')
 const router= express.Router();
 
 router.route('/sign-up').post(userController.signUp);
+router.route('/update/:id').patch(userController.updateUser)
+router.route('/delete/:id').delete(userController.deleteUser)
 
 module.exports= router
