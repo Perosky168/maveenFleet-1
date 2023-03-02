6c9a2938d18a68704bf12394f52ecb1b89774e26
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2023-03-02 20:21:22 +0100

    routes for messages

--- Routes/messageRoute.js
+++ Routes/messageRoute.js
@@ -1,8 +1,14 @@
 const express= require('express');
 const messageController= require('../Controllers/messageController')
+const authController= require('../Controllers/authController')
 
 router= express.Router()
 
 router.route('/send-message').post(messageController.sendMessage);
+router.route('/update/:id').patch(messageController.updateMessage)
+
+router.use(authController.protect, authController.restrictTo("admin"))
+
+router.route('/delete/:id').delete(messageController.deleteMessage)
 
 module.exports= router
