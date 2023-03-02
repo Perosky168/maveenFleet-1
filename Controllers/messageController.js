55b7294371ef0886bb27e4f1f7b31fef2f517b0e
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2023-03-02 20:21:36 +0100

    message functions

--- Controllers/messageController.js
+++ Controllers/messageController.js
@@ -4,15 +4,32 @@ const catchAsync= require('../utils/catchAsync');
 
 exports.sendMessage= catchAsync(async(req, res,next)=>{
     const message= await Message.create(req.body)
-    message.firstName= req.body.fullname[0]
-    const url= `${req.protocol}://${req.get('host')}/class/${message.id}`
 
-    await new Email(message,url).messageSentAlert()
 
     res.status(200).json({
         status: "success",
         message
     })
 
+});
+
+exports.updateMessage= catchAsync(async(req,res,next)=>{
+    const message= await Message.findByIdAndUpdate(req.params.id, req.body, {
+        new:true,
+        runValidators:true
+    });
+    res.status(200).json({
+        status: "sucess",
+        message
+    })
+
+});
+
+exports.deleteMessage= catchAsync(async (req,res,next)=>{
+    await Message.findByIdAndDelete(req.params.id)
+
+    res.status(200).json({
+        status: "success",
+    })
 })
 
