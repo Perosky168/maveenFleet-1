9c00d47ba9008723b943216bc89c82822da25044
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2023-03-07 20:20:36 +0100

    version 1.0.2

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -35,6 +35,14 @@ const sendErrorDev= (err,res)=>{
         message: err.message,
         stack: err.stack
     })
+
+    console.error('ERROR 💥', err);
+    return res.status(err.statusCode).render('error', {
+      title: 'Something went wrong!',
+      msg: err.message
+    });
+
+    
 };
 
 const sendProdErr= (err, res)=>{
@@ -55,7 +63,14 @@ const sendProdErr= (err, res)=>{
             status: 'error',
             message: 'Something went very wrong'
         })
-    };
+    }
+
+    if (err.isOperational) {
+      return res.status(err.statusCode).render('error', {
+        title: 'Something went wrong!',
+        msg: "Try Again"
+      });
+    }
 
 }
 
--- Models/userModel.js
+++ Models/userModel.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now()
-    }
+    },
 });
 
 const User = mongoose.model('User', userSchema);
